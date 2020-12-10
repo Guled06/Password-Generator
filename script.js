c05024558d004349682a97095bff4aeab3cb4131
Guled Ahmed <akindnesstoremember@gmail.com>
2020-12-10 15:13:24 -0800

    working on homework

--- script.js
+++ script.js
@@ -8,6 +8,30 @@ function writePassword() {
 
   passwordText.value = password;
 
+  var passwordFresh = []
+
+  var passWord = prompt("Enter a password length");
+  passWord = parseInt(passWord);
+
+  if (passWord < 8) {
+    alert ("Password must have more than 7");
+    return "";
+  }
+
+  if (passWord > 128) {
+    alert("Password must be less than 128");
+    return "";
+  }
+
+  var lowerCase = confirm("Would you like lower case letters to be included in your password?");
+
+  var upperCase = confirm("Would you like upper case letters to be included in your password?");
+
+  var numbers = confirm("Would you like numbers to be included in your password?");
+
+  var specialCharacters = confirm("Would you like special characters included in your password?");
+
+
 }
 
 // Add event listener to generate button
