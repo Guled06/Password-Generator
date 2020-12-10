60a8f63614d54571206bd8418e8394215c1fc833
Guled Ahmed <akindnesstoremember@gmail.com>
2020-12-09 16:43:03 -0800

    add new js file, deleted the old one

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
 // Assignment Code
-
-
 var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  return "password";
-}
 // Write password to the #password input
 function writePassword() {
-
-  parseInt(prompt("Please choose between 5 and 10"));
-  
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
-
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
