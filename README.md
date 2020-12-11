d813fbac028841a307cb53e09fbbcfb9746c84e9
Guled Ahmed <akindnesstoremember@gmail.com>
2020-12-10 17:29:00 -0800

    adding code and comments to the script.js and readme.md files

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Password-Generator
+
+This is was our third homework assigned to us in our second week of the UCSD Triology Coding Program. In this homework, we were tasked to create an application that will allow the user to generate a random password based on the criteria they have chosen to create their new password. This app was design to run the browser using Index.html file, a style.css file and a script.js file. 
+
+In the script.js, I created a variable and named it "AlphaBet". I gave this variable a string containing the alphabets in lower case from a-z. 
+
+I also created a variable and gave this variable a string containing the alphabets in upper case from A-Z. 
+
+I created a variable and gave this variable a string containing 
--- script.js
+++ script.js
@@ -1,6 +1,75 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var alphaBet = "abcdefghijklmnopqrstuvwxyz"
+
+var lettErs = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+var digiTs = "0123456789"
+
+
+function generatePassword () {
+  var newPasswordArray = [];
+
+  var passwordFresh = prompt("Please enter a password length");
+  passwordFresh = parseInt(passwordFresh);
+
+  if (passwordFresh < 8) {
+    alert ("Password must have more than 7 characters:");
+    return "";
+  }
+
+  if (passwordFresh > 128) {
+    alert("Password must not be more than 128 characters:");
+    return "";
+  }
+
+  var lowerCase = confirm("Would you like lower case letters to be included in your password?");
+
+  var upperCase = confirm("Would you like upper case letters to be included in your password?");
+
+  var numbers = confirm("Would you like numbers to be included in your password?");
+
+  var passwordOptions = [];
+
+  if (lowerCase) {
+    passwordOptions.push("lowercase");
+  }
+
+  if (upperCase) {
+    passwordOptions.push("uppercase");
+  }
+
+  if (numbers) {
+    passwordOptions.push("numbers");
+  }
+
+  
+  for (var i = 0; i < passwordFresh; i++) {
+
+    var randomCharacter = passwordOptions[Math.floor(Math.random() * passwordOptions.length)];
+
+    if (randomCharacter === "numbers") {
+      
+      var randomNumber = digiTs.charAt(Math.floor(Math.random() * digiTs.length));
+      newPasswordArray.push(randomNumber); 
+    } else if (randomCharacter === "uppercase") {
+      var randomUper = lettErs.charAt(Math.floor(Math.random() * lettErs.length));
+      newPasswordArray.push(randomUper)
+    } else if (randomCharacter === "lowercase") {
+      var randomLower = alphaBet.charAt(Math.floor(Math.random() * alphaBet.length));
+      newPasswordArray.push(randomLower)
+    } console.log(newPasswordArray);
+  } return newPasswordArray.join("");
+
+
+}
+
+
+
+
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
